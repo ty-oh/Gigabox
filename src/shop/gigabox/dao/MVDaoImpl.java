9c71a3ec365563070bb68d0e30acc7ff3c4e8b45
ty-oh <proftyo@gmail.com>
2022-12-05 13:21:22 +0900

    메인페이지 헤더 구현 및 영화 등록 기능 구현

---
+++ src/shop/gigabox/dao/MVDaoImpl.java
@@ -0,0 +1,35 @@
+package shop.gigabox.dao;
+
+import org.apache.ibatis.session.SqlSession;
+
+import shop.gigabox.mybatis.config.DBService;
+import shop.gigabox.vo.MVVO;
+
+public class MVDaoImpl implements MVDao{
+	private static MVDaoImpl instance = null;
+	private MVDaoImpl() {}
+	public static MVDaoImpl getInstance() {
+		if (instance == null) {
+			instance = new MVDaoImpl();
+		}
+		return instance;
+	}
+	
+	private static SqlSession sqlSession = null; 
+	
+	private synchronized static SqlSession getSqlSession() {
+		if (sqlSession == null) {
+			sqlSession = DBService.getFactory().openSession(false);
+		}
+		return sqlSession;
+	}
+	
+	@Override
+	public int insert(MVVO mvvo) {
+		int result = getSqlSession().insert("insert_mv", mvvo);
+		if(result > 0) {
+			getSqlSession().commit();
+		}
+		return result;
+	}
+}
---
+++ src/shop/gigabox/service/MVServiceImpl.java
@@ -0,0 +1,14 @@
+package shop.gigabox.service;
+
+import shop.gigabox.dao.MVDao;
+import shop.gigabox.dao.MVDaoImpl;
+import shop.gigabox.vo.MVVO;
+
+public class MVServiceImpl implements MVService {
+	MVDao mvdao = MVDaoImpl.getInstance();
+	
+	@Override
+	public int insert(MVVO mvvo) {
+		return mvdao.insert(mvvo);
+	}
+}
